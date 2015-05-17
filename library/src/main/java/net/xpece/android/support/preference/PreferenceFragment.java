8ad2b677c0fb42edb1d3640dda85213577fd7eb3
consp1racy <e.pechanec@gmail.com>
2015-05-17 21:55:34 +0200

    Version 0.2.1 FIXED: No need to manually prefix preferences ... even in fragments

--- library/src/main/java/net/xpece/android/support/preference/PreferenceFragment.java
+++ library/src/main/java/net/xpece/android/support/preference/PreferenceFragment.java
@@ -2,6 +2,7 @@ package net.xpece.android.support.preference;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -24,4 +25,34 @@ public class PreferenceFragment extends android.preference.PreferenceFragment {
         list.setSelector(Util.resolveDrawable(context, R.attr.selectableItemBackground));
         list.setCacheColorHint(0);
     }
+
+    /**
+     * Adds preferences from activities that match the given {@link Intent}.
+     *
+     * @param intent The {@link Intent} to query activities.
+     */
+    public void addPreferencesFromIntent(Intent intent) {
+        requirePreferenceManager();
+
+        setPreferenceScreen(PreferenceManagerCompat.inflateFromIntent(getPreferenceManager(), getActivity(), intent, getPreferenceScreen()));
+    }
+
+    /**
+     * Inflates the given XML resource and adds the preference hierarchy to the current
+     * preference hierarchy.
+     *
+     * @param preferencesResId The XML resource ID to inflate.
+     */
+    public void addPreferencesFromResource(int preferencesResId) {
+        requirePreferenceManager();
+
+        setPreferenceScreen(PreferenceManagerCompat.inflateFromResource(getPreferenceManager(), getActivity(), preferencesResId, getPreferenceScreen()));
+    }
+
+    private void requirePreferenceManager() {
+        if (getPreferenceManager() == null) {
+            throw new RuntimeException("This should be called after super.onCreate.");
+        }
+    }
+
 }
