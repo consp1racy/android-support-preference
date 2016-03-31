363d8791ad48d33831f02d15427af046c39f2b30
consp1racy <e.pechanec@gmail.com>
2016-03-31 13:17:31 +0200

    Consistent XpPreferenceDialogFragment theme resolution

--- library/src/main/java/net/xpece/android/support/preference/XpPreferenceDialogFragment.java
+++ library/src/main/java/net/xpece/android/support/preference/XpPreferenceDialogFragment.java
@@ -1,18 +1,19 @@
 package net.xpece.android.support.preference;
 
 import android.content.Context;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.preference.PreferenceDialogFragmentCompat;
-import android.view.ContextThemeWrapper;
 import android.view.View;
 
 /**
  * Created by Eugen on 26.03.2016.
  */
 public abstract class XpPreferenceDialogFragment extends PreferenceDialogFragmentCompat {
+
     @Override
     protected View onCreateDialogView(Context context) {
-        int alertDialogTheme = Util.resolveResourceId(context, R.attr.alertDialogTheme, 0);
-        context = new ContextThemeWrapper(context, alertDialogTheme);
-        return super.onCreateDialogView(context);
+        Context context2 = new AlertDialog.Builder(context).getContext();
+//        Context context2 = new AlertDialog.Builder(context, getTheme()).getContext();
+        return super.onCreateDialogView(context2);
     }
 }
