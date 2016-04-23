bbc1915b5f368f922d050c15eea9c9d968c7e786
consp1racy <e.pechanec@gmail.com>
2016-04-23 19:55:45 +0200

    Version 0.8.0

--- spinner/src/main/java/net/xpece/android/support/widget/CheckedItemAdapter.java
+++ spinner/src/main/java/net/xpece/android/support/widget/CheckedItemAdapter.java
@@ -25,6 +25,13 @@ public class CheckedItemAdapter extends ArrayAdapter<CharSequence> implements Th
 
     private int mSelection = -1;
 
+    public static CheckedItemAdapter newInstance(Context context, CharSequence[] objects, int selection) {
+        CheckedItemAdapter a = new CheckedItemAdapter(context, android.R.layout.simple_spinner_item, android.R.id.text1, objects);
+        a.setDropDownViewResource(R.layout.asp_simple_spinner_dropdown_item);
+        a.setSelection(selection);
+        return a;
+    }
+
     public CheckedItemAdapter(Context context, int resource, int textViewResourceId,
                               CharSequence[] objects) {
         super(context, resource, textViewResourceId, objects);
