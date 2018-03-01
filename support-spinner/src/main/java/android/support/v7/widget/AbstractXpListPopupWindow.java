consp1racy/android-support-preference

commit 168a8a445d1bade458d79ce46f85557fa7840939

DO-NOT-MERGE: XpDropDownListView doesn't crash with support lib 27.1.0
TODO: Popup position is broken. Fix pls

support-spinner/src/main/java/android/support/v7/widget/AbstractXpListPopupWindow.java
@@ -158,6 +158,8 @@ public abstract class AbstractXpListPopupWindow implements ShowableListMenu {
 
     private int mLayoutDirection;
 
+    private int mMaxMeasuredItems = Integer.MAX_VALUE;
+
     /**
      * The provided prompt view should appear above list content.
      *
@@ -382,6 +384,10 @@ public abstract class AbstractXpListPopupWindow implements ShowableListMenu {
         }
         int widthSpec = MeasureSpec.makeMeasureSpec(getListWidthSpec(), MeasureSpec.AT_MOST);
         return mDropDownList.measureHeightOfChildrenCompat(widthSpec, fromIncl, toExcl, Integer.MAX_VALUE, 1);
+    }
+
+    public void setMaxMeasuredItems(final int maxMeasuredItems) {
+        mMaxMeasuredItems = maxMeasuredItems;
     }
 
     /**
@@ -1194,7 +1200,7 @@ public abstract class AbstractXpListPopupWindow implements ShowableListMenu {
                 widthSpec = mDropDownMaxWidth - mps;
             }
         } else if (mDropDownWidth == WIDTH_WRAP_CONTENT_UNIT) {
-            int preferredWidth = mDropDownList.compatMeasureContentWidth() + getBackgroundHorizontalPadding();
+            int preferredWidth = mDropDownList.compatMeasureContentWidth(mMaxMeasuredItems) + getBackgroundHorizontalPadding();
             if (mDropDownPreferredWidthUnit > 0) {
                 int units = (int) Math.ceil(preferredWidth / mDropDownPreferredWidthUnit);
                 if (units == 1) {
@@ -2024,12 +2030,11 @@ public abstract class AbstractXpListPopupWindow implements ShowableListMenu {
                 break;
         }
 
-        final int listPadding = mDropDownList.getPaddingTop() + mDropDownList.getPaddingBottom();
         final int listContent = mDropDownList.measureHeightOfChildrenCompat(childWidthSpec,
-                0, mMaxItemCount, maxHeight - otherHeights - verticalMargin - listPadding + padding, -1);
+                0, mMaxItemCount, maxHeight - otherHeights - verticalMargin + padding, -1);
         // add padding only if the list has items in it, that way we don't show
         // the popup if it is not needed
-        if (otherHeights > 0 || listContent > 0) otherHeights += padding + listPadding;
+        if (otherHeights > 0 || listContent > 0) otherHeights += padding;
 
         final int result = listContent + otherHeights;
         mListMeasuredHeight = result;

build.gradle
@@ -12,7 +12,7 @@ buildscript {
 ext {
     compileSdkVersion = 27
     buildToolsVersion = "27.0.3"
-    supportLibVersion = "27.0.2"
+    supportLibVersion = "27.1.0"
     supportLibVersionAnnotations = supportLibVersion
     minSdkVersion = 14
     targetSdkVersion = 27

support-preference/src/main/java/net/xpece/android/support/preference/ListPreference.java
@@ -321,6 +321,8 @@ public class ListPreference extends DialogPreference {
 
         mSimpleMenuShowing = true;
 
+        onPreShowPopup(popup);
+
         popup.show();
 
         final ListView list = popup.getListView();
@@ -331,6 +333,17 @@ public class ListPreference extends DialogPreference {
         popup.setSelection(position);
 
         return true;
+    }
+
+    /**
+     * Last chance to modify the popup window before showing.
+     *
+     * You can use this e.g. to {@link XpListPopupWindow#setMaxMeasuredItems(int)} if your data set
+     * is larger than 15 items, to conserve processing power.
+     *
+     * @param popup Popup to be displayed
+     */
+    public void onPreShowPopup(final XpListPopupWindow popup) {
     }
 
     /**

support-spinner/src/main/java/android/support/v7/widget/AbstractXpAppCompatSpinner.java
@@ -63,6 +63,9 @@ import static android.support.annotation.RestrictTo.Scope.LIBRARY_GROUP;
 @TargetApi(23)
 public abstract class AbstractXpAppCompatSpinner extends Spinner implements TintableBackgroundView {
 
+    //    private static final int MAX_ITEMS_MEASURED = 15;
+    private static final int MAX_ITEMS_MEASURED = Integer.MAX_VALUE;
+
     private static final boolean IS_AT_LEAST_K = Build.VERSION.SDK_INT >= 19;
     private static final boolean IS_AT_LEAST_M = Build.VERSION.SDK_INT >= 23;
 
@@ -384,13 +387,16 @@ public abstract class AbstractXpAppCompatSpinner extends Spinner implements Tint
 
     private int measureContentWidth() {
         final SpinnerAdapter adapter = getAdapter();
+
         if (adapter == null) {
-            return 0;
+            return getPaddingLeft() + getPaddingRight();
         }
 
         int width = 0;
-        View itemView = null;
+
+        View child = null;
         int itemType = 0;
+
         final int widthMeasureSpec =
                 makeSafeMeasureSpec(getMeasuredWidth(), MeasureSpec.UNSPECIFIED);
         final int heightMeasureSpec =
@@ -399,24 +405,34 @@ public abstract class AbstractXpAppCompatSpinner extends Spinner implements Tint
         // Make sure the number of items we'll measure is capped. If it's a huge data set
         // with wildly varying sizes, oh well.
         final int maxItemsMeasured = getMaxItemsMeasured();
+
         int start = Math.max(0, getSelectedItemPosition());
         final int end = Math.min(adapter.getCount(), start + maxItemsMeasured);
         final int count = end - start;
         start = Math.max(0, start - (maxItemsMeasured - count));
         for (int i = start; i < end; i++) {
-            final int positionType = adapter.getItemViewType(i);
-            if (positionType != itemType) {
-                itemType = positionType;
-                itemView = null;
+            final int newType = adapter.getItemViewType(i);
+            if (newType != itemType) {
+                itemType = newType;
+                child = null;
             }
-            itemView = adapter.getView(i, itemView, this);
-            if (itemView.getLayoutParams() == null) {
-                itemView.setLayoutParams(new ViewGroup.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT));
+            child = adapter.getView(i, child, this);
+
+            ViewGroup.LayoutParams childLp = child.getLayoutParams();
+            if (childLp == null) {
+                childLp = generateDefaultLayoutParams();
+                child.setLayoutParams(childLp);
             }
-            itemView.measure(widthMeasureSpec, heightMeasureSpec);
-            width = Math.max(width, itemView.getMeasuredWidth());
+            childLp.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+            childLp.width = ViewGroup.LayoutParams.WRAP_CONTENT;
+
+            child.measure(widthMeasureSpec, heightMeasureSpec);
+
+            // Since this view was measured directly against the parent measure
+            // spec, we must measure it again before reuse.
+            child.forceLayout();
+
+            width = Math.max(width, child.getMeasuredWidth());
         }
 
         width += getPaddingLeft() + getPaddingRight();
@@ -446,10 +462,7 @@ public abstract class AbstractXpAppCompatSpinner extends Spinner implements Tint
         return MeasureSpec.makeMeasureSpec(size, mode);
     }
 
-    /**
-     * Match behavior of spinner with {@link XpDropDownListView}.
-     */
-    private int getMaxItemsMeasured() {
-        return XpDropDownListView.MAX_ITEMS_MEASURED;
+    protected int getMaxItemsMeasured() {
+        return MAX_ITEMS_MEASURED;
     }
 }

commit 8041daa0cf8fadf73356e97ba6699ef3bd30812a

Fixed regressions since 168a8a4
Fixed DropDownListView clipToPadding false not applied.
Fixed vertical positioning which requires resolved list padding.

Optimized list padding resolution.

sample/src/main/AndroidManifest.xml
@@ -2,14 +2,14 @@
 <manifest
     package="net.xpece.android.support.preference.sample"
     xmlns:android="http://schemas.android.com/apk/res/android"
-    xmlns:tools="http://schemas.android.com/tools">
+    >
 
     <!-- Reading Ringtones on API 11 or 14 -->
     <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE"/>
 
     <application
-        android:allowBackup="true"
         android:name=".SampleApplication"
+        android:allowBackup="true"
         android:icon="@mipmap/ic_launcher"
         android:label="@string/app_name"
         android:supportsRtl="true"
@@ -17,6 +17,7 @@
 
         <activity
             android:name=".SettingsActivity"
+            android:exported="true"
             android:label="@string/settings_activity_title"
             android:theme="@style/AppTheme.NoActionBar">
             <intent-filter>
@@ -26,6 +27,7 @@
         </activity>
         <activity
             android:name=".SpinnerActivity"
+            android:exported="true"
             android:label="@string/spinner_sample"
             android:parentActivityName=".SettingsActivity"
             android:theme="@style/AppTheme.NoActionBar">

support-spinner/src/main/java/android/support/v7/widget/AbstractXpListPopupWindow.java
@@ -1397,6 +1397,7 @@ public abstract class AbstractXpListPopupWindow implements ShowableListMenu {
 
     private void setSelectionOverAnchor(
             final XpDropDownListView list, final int position, final int offsetY) {
+
         final View anchor = getAnchorView();
 
         final int listTop = mComputedPopupY + getBackgroundTopPadding();
@@ -1406,6 +1407,9 @@ public abstract class AbstractXpListPopupWindow implements ShowableListMenu {
         final int anchorHeight = anchor.getHeight() - anchorPaddingTop - anchor.getPaddingBottom();
         final int itemHeight = getSelectedItemViewHeight(position);
         final int anchorInset = (anchorHeight - itemHeight) / 2 + anchorPaddingTop;
+
+        // Before setting selection make sure list padding is resolved.
+        list.ensureListPaddingResolved();
 
         final int realOffsetY = anchorTop - listTop + anchorInset
                 + offsetY // Apply user supplied offset.
@@ -1494,8 +1498,11 @@ public abstract class AbstractXpListPopupWindow implements ShowableListMenu {
             position = Math.max(0, position - mAdapter.getCount() + maxLength);
         }
 
+        // Before measuring list padding make sure it is resolved.
+        mDropDownList.ensureListPaddingResolved();
+
         final int viewHeight = anchor.getHeight();
-        final int dropDownListViewPaddingTop = mDropDownList.getPaddingTop();
+        final int dropDownListViewPaddingTop = mDropDownList.getListPaddingTop();
         final int selectedItemHeight = popup.measureItem(position);
         final int beforeSelectedItemHeight = popup.measureItems(realPosition - position, realPosition + 1);
 
@@ -2025,11 +2032,12 @@ public abstract class AbstractXpListPopupWindow implements ShowableListMenu {
                 break;
         }
 
+        final int listPadding = mDropDownList.getPaddingTop() + mDropDownList.getPaddingBottom();
         final int listContent = mDropDownList.measureHeightOfChildrenCompat(childWidthSpec,
-                0, mMaxItemCount, maxHeight - otherHeights - verticalMargin + padding, -1);
+                0, mMaxItemCount, maxHeight - otherHeights - verticalMargin - listPadding + padding, -1);
         // add padding only if the list has items in it, that way we don't show
         // the popup if it is not needed
-        if (otherHeights > 0 || listContent > 0) otherHeights += padding;
+        if (otherHeights > 0 || listContent > 0) otherHeights += padding + listPadding;
 
         final int result = listContent + otherHeights;
         mListMeasuredHeight = result;

support-spinner/src/main/java/android/support/v7/widget/XpDropDownListView.java
@@ -2,19 +2,27 @@ package android.support.v7.widget;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.v4.util.LruCache;
 import android.support.v4.view.ViewCompat;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListAdapter;
 
+import net.xpece.android.support.widget.spinner.R;
+
 @SuppressLint("ViewConstructor")
 public final class XpDropDownListView extends DropDownListView {
+
+    private static final int[] ATTRS = new int[]{android.R.attr.clipToPadding};
 
     private LruCache<Integer, View> mMeasuredViewCache = new LruCache<>(2);
 
     private boolean mHasMultiLineItems;
+
+    private boolean mResolvingListPadding = false;
 
     /**
      * <p>Creates a new list view wrapper.</p>
@@ -23,6 +31,14 @@ public final class XpDropDownListView extends DropDownListView {
      */
     XpDropDownListView(final Context context, final boolean hijackFocus) {
         super(context, hijackFocus);
+
+        if (Build.VERSION.SDK_INT < 21) {
+            // For the love of god clipToPadding just cannot be read on the first try on Android 4.
+            TypedArray a = context.obtainStyledAttributes(null, ATTRS, R.attr.dropDownListViewStyle, 0);
+            final boolean clipToPadding = a.getBoolean(0, true);
+            a.recycle();
+            setClipToPadding(clipToPadding);
+        }
     }
 
     boolean hasMultiLineItems() {
@@ -59,21 +75,18 @@ public final class XpDropDownListView extends DropDownListView {
     public int measureHeightOfChildrenCompat(int widthMeasureSpec, int startPosition,
                                              int endPosition, final int maxHeight,
                                              int disallowPartialChildPosition) {
-        ensureListPaddingResolved();
 
-        final int paddingTop = getListPaddingTop();
-        final int paddingBottom = getListPaddingBottom();
         final int reportedDividerHeight = getDividerHeight();
         final Drawable divider = getDivider();
 
         final ListAdapter adapter = getAdapter();
 
         if (adapter == null) {
-            return paddingTop + paddingBottom;
+            return 0;
         }
 
         // Include the padding of the list
-        int returnedHeight = paddingTop + paddingBottom;
+        int returnedHeight = 0;
         final int dividerHeight = ((reportedDividerHeight > 0) && divider != null)
                 ? reportedDividerHeight : 0;
 
@@ -161,7 +174,7 @@ public final class XpDropDownListView extends DropDownListView {
 
     /**
      * Compute preferred, or unconstrained, width of the largest child view plus list padding.
-     *
+     * <p>
      * Note: This method measures all items in the adapter, don't use with large data sets.
      *
      * @return Preferred width of the list view
@@ -217,19 +230,44 @@ public final class XpDropDownListView extends DropDownListView {
         return returnedWidth;
     }
 
-    /**
-     * {@code #getListPadding*()} returns incorrect number before the first measurement.
-     * Call this method to manually resolve list padding.
-     */
-    public void ensureListPaddingResolved() {
-        final int transcriptMode = getTranscriptMode();
-        setTranscriptMode(TRANSCRIPT_MODE_DISABLED);
-        measure(0, 0);
-        setTranscriptMode(transcriptMode);
-    }
-
     private int getAllHorizontalPadding() {
         return getListPaddingLeft() + getListPaddingRight();
     }
 
+    /**
+     * {@code #getListPadding*()} returns incorrect number before the first measurement.
+     * Call this method to manually resolve list padding.
+     */
+    @SuppressLint("WrongCall")
+    public void ensureListPaddingResolved() {
+        mResolvingListPadding = true;
+        final int transcriptMode = getTranscriptMode();
+        try {
+            // Make sure we intercept some call and terminate measurement after what we need.
+            // The first call is #getChildCount when transcript mode is normal.
+            setTranscriptMode(TRANSCRIPT_MODE_NORMAL);
+
+            // List padding is resolved as part of AbsListView#onMeasure.
+            // Do NOT call measure! We're not actually measuring and setting measured dimension.
+            onMeasure(0, 0);
+        } catch (StopExecution ignored) {
+            // See comments in getChildCount().
+        } finally {
+            setTranscriptMode(transcriptMode);
+            mResolvingListPadding = false;
+        }
+    }
+
+    @Override
+    public int getChildCount() {
+        if (mResolvingListPadding) {
+            throw new StopExecution();
+        }
+        return super.getChildCount();
+    }
+
+    private class StopExecution extends RuntimeException {
+        StopExecution() {
+        }
+    }
 }