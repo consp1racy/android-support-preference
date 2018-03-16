36bee503dde529ab85547c06618a0699e3df837e
Eugen Pechanec <e.pechanec@gmail.com>
2018-03-16 17:05:20 +0100

    Fix popup exit transition on API 24 and 25

--- support-spinner/src/main/java/android/support/v7/widget/XpAppCompatPopupWindow.java
+++ support-spinner/src/main/java/android/support/v7/widget/XpAppCompatPopupWindow.java
@@ -1,6 +1,7 @@
 package android.support.v7.widget;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Build;
@@ -9,6 +10,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.annotation.StyleRes;
+import android.support.v4.view.ViewCompat;
 import android.transition.Transition;
 import android.transition.TransitionInflater;
 import android.transition.TransitionSet;
@@ -28,12 +30,20 @@ class XpAppCompatPopupWindow extends AppCompatPopupWindow {
     private static final int ATTR_POPUP_EXIT_TRANSITION = 0x01010520;
     private static final int[] ATTRS = {ATTR_POPUP_ENTER_TRANSITION, ATTR_POPUP_EXIT_TRANSITION};
 
+    // Exit transition doesn't work on API 24-25 unless anchor root attached state is in sync.
+    // Otherwise only mAnchor would be enough for <insert when you remember>.
     private static final Field sAnchorField;
+    private static final Field sAnchorRootField;
+    private static final Field sIsAnchorRootAttachedField;
+    private static final Field sOnAnchorRootDetachedListenerField;
+
+//    private static final boolean SHOULD_FIX_TRANSITION = Build.VERSION.SDK_INT >= 23;
+    private static final boolean SHOULD_FIX_TRANSITION = Build.VERSION.SDK_INT == 24 || Build.VERSION.SDK_INT == 25;
 
     static {
         final Class<PopupWindow> cls = PopupWindow.class;
-
         Field f = null;
+
         try {
             f = cls.getDeclaredField("mAnchor");
             f.setAccessible(true);
@@ -41,6 +51,36 @@ class XpAppCompatPopupWindow extends AppCompatPopupWindow {
             Log.i(TAG, "Could not find field mAnchor on PopupWindow. Oh well.");
         }
         sAnchorField = f;
+
+        if (SHOULD_FIX_TRANSITION) {
+            try {
+                f = cls.getDeclaredField("mAnchorRoot");
+                f.setAccessible(true);
+            } catch (NoSuchFieldException e) {
+                Log.i(TAG, "Could not find field mAnchorRoot on PopupWindow. Oh well.");
+            }
+            sAnchorRootField = f;
+
+            try {
+                f = cls.getDeclaredField("mIsAnchorRootAttached");
+                f.setAccessible(true);
+            } catch (NoSuchFieldException e) {
+                Log.i(TAG, "Could not find field mIsAnchorRootAttached on PopupWindow. Oh well.");
+            }
+            sIsAnchorRootAttachedField = f;
+
+            try {
+                f = cls.getDeclaredField("mOnAnchorRootDetachedListener");
+                f.setAccessible(true);
+            } catch (NoSuchFieldException e) {
+                Log.i(TAG, "Could not find field mOnAnchorRootDetachedListener on PopupWindow. Oh well.");
+            }
+            sOnAnchorRootDetachedListenerField = f;
+        } else {
+            sAnchorRootField = null;
+            sIsAnchorRootAttachedField = null;
+            sOnAnchorRootDetachedListenerField = null;
+        }
     }
 
     private final Context mApplicationContext;
@@ -88,11 +128,38 @@ class XpAppCompatPopupWindow extends AppCompatPopupWindow {
         setAnchorInternal(anchor);
     }
 
-    private void setAnchorInternal(@Nullable View anchor) {
+    @TargetApi(23)
+    private void setAnchorInternal(@NonNull final View anchor) {
+        if (SHOULD_FIX_TRANSITION) {
+            setAnchorMarshmallow(anchor);
+        } else {
+            setAnchorLegacy(anchor);
+        }
+    }
+
+    private void setAnchorLegacy(@NonNull final View anchor) {
         try {
             sAnchorField.set(this, new WeakReference<>(anchor));
         } catch (Exception e) {
-            Log.i(TAG, "Could not set mAnchor on PopupWindow. Oh well.");
+            Log.i(TAG, "Could not set anchor on PopupWindow. Oh well.");
+        }
+    }
+
+    @RequiresApi(23)
+    private void setAnchorMarshmallow(@NonNull final View anchor) {
+        final View anchorRoot = anchor.getRootView();
+        final boolean isAnchorRootAttached = ViewCompat.isAttachedToWindow(anchorRoot);
+
+        try {
+            final View.OnAttachStateChangeListener listener =
+                    (View.OnAttachStateChangeListener) sOnAnchorRootDetachedListenerField.get(this);
+            anchorRoot.addOnAttachStateChangeListener(listener);
+
+            sAnchorField.set(this, new WeakReference<>(anchor));
+            sAnchorRootField.set(this, new WeakReference<>(anchorRoot));
+            sIsAnchorRootAttachedField.set(this, isAnchorRootAttached);
+        } catch (Exception e) {
+            Log.i(TAG, "Could not set anchor on PopupWindow. Oh well.");
         }
     }
 
