badd1f5f49387fc6d87d22cb6ae359402fb1cc29
Eugen Pechanec <e.pechanec@gmail.com>
2018-04-18 14:21:14 +0200

    Simplify XpPreferenceGroupAdapter, rely on super implementation

--- support-preference/src/main/java/android/support/v7/preference/XpPreferenceGroupAdapter.java
+++ support-preference/src/main/java/android/support/v7/preference/XpPreferenceGroupAdapter.java
@@ -1,14 +1,9 @@
 package android.support.v7.preference;
 
-import android.text.TextUtils;
-import android.view.LayoutInflater;
-import android.view.View;
+import android.support.annotation.NonNull;
 import android.view.ViewGroup;
 
 import net.xpece.android.support.preference.ColorableTextPreference;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Eugen on 17. 3. 2016.
@@ -17,10 +12,6 @@ import java.util.List;
 class XpPreferenceGroupAdapter extends PreferenceGroupAdapter {
 
     private static final int OFFSET = 0xffff;
-
-    private final List<PreferenceLayout> mPreferenceLayouts = new ArrayList<>();
-
-    private PreferenceLayout mTempPreferenceLayout = new PreferenceLayout();
 
     public XpPreferenceGroupAdapter(final PreferenceGroup preferenceGroup) {
         super(preferenceGroup);
@@ -45,54 +36,25 @@ class XpPreferenceGroupAdapter extends PreferenceGroupAdapter {
                 offset += OFFSET;
             }
         }
-        return offset + getItemViewTypeOriginal(position);
+        return offset + superGetItemViewType(position);
     }
 
-    public int getItemViewTypeOriginal(int position) {
-        Preference preference = this.getItem(position);
-        this.mTempPreferenceLayout = this.createPreferenceLayout(preference, this.mTempPreferenceLayout);
-        int viewType = this.mPreferenceLayouts.indexOf(this.mTempPreferenceLayout);
-        if (viewType != -1) {
-            return viewType;
-        } else {
-            viewType = this.mPreferenceLayouts.size();
-            this.mPreferenceLayouts.add(new PreferenceLayout(this.mTempPreferenceLayout));
-            return viewType;
-        }
+    private int superGetItemViewType(int position) {
+        return super.getItemViewType(position);
     }
 
     @Override
-    public void onViewDetachedFromWindow(PreferenceViewHolder holder) {
+    public void onViewDetachedFromWindow(@NonNull PreferenceViewHolder holder) {
         super.onViewDetachedFromWindow(holder);
 
+        // Clear key listener from SeekBarPreference.
         holder.itemView.setOnKeyListener(null);
     }
 
+    @NonNull
     @Override
-    public PreferenceViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        viewType %= OFFSET;
-
-        PreferenceLayout pl = this.mPreferenceLayouts.get(viewType);
-        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View view = inflater.inflate(pl.resId, parent, false);
-        ViewGroup widgetFrame = (ViewGroup) view.findViewById(android.R.id.widget_frame);
-        if (widgetFrame != null) {
-            if (pl.widgetResId != 0) {
-                inflater.inflate(pl.widgetResId, widgetFrame);
-            } else {
-                widgetFrame.setVisibility(View.GONE);
-            }
-        }
-
-        return new PreferenceViewHolder(view);
-    }
-
-    private PreferenceLayout createPreferenceLayout(Preference preference, PreferenceLayout in) {
-        PreferenceLayout pl = in != null ? in : new PreferenceLayout();
-        pl.name = preference.getClass().getName();
-        pl.resId = preference.getLayoutResource();
-        pl.widgetResId = preference.getWidgetLayoutResource();
-        return pl;
+    public PreferenceViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return super.onCreateViewHolder(parent, viewType % OFFSET);
     }
 
     @Override
@@ -101,37 +63,5 @@ class XpPreferenceGroupAdapter extends PreferenceGroupAdapter {
 
         final Preference preference = getItem(position);
         XpPreferenceHelpers.onBindViewHolder(preference, holder);
-    }
-
-    private static class PreferenceLayout {
-        int resId;
-        int widgetResId;
-        String name;
-
-        public PreferenceLayout() {
-        }
-
-        public PreferenceLayout(PreferenceLayout other) {
-            this.resId = other.resId;
-            this.widgetResId = other.widgetResId;
-            this.name = other.name;
-        }
-
-        public boolean equals(Object o) {
-            if (!(o instanceof PreferenceLayout)) {
-                return false;
-            } else {
-                PreferenceLayout other = (PreferenceLayout) o;
-                return this.resId == other.resId && this.widgetResId == other.widgetResId && TextUtils.equals(this.name, other.name);
-            }
-        }
-
-        public int hashCode() {
-            byte result = 17;
-            int result1 = 31 * result + this.resId;
-            result1 = 31 * result1 + this.widgetResId;
-            result1 = 31 * result1 + this.name.hashCode();
-            return result1;
-        }
     }
 }
